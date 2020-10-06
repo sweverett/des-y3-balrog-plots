d80f4e5165322cd6b2e5c0ba4e7b828e4bc61054
Spencer <spencerweverett@gmail.com>
2020-10-06 16:57:05 -0500

    Added bin info for new maps

--- systematics-maps-compare/bin_info.py
+++ systematics-maps-compare/bin_info.py
@@ -11,14 +11,26 @@ density_xlim = {
     'depth_r' : [22, 24],
     'depth_i' : [22, 24],
     'depth_z' : [22, 24],
+    'skyvar_g' : [5, 15],
+    'skyvar_r' : [5, 15],
     'skyvar_i' : [5, 15],
+    'skyvar_z' : [5, 15],
+    'skybrite_g' : [2000, 5000],
+    'skybrite_r' : [2000, 5000],
     'skybrite_i' : [2000, 5000],
+    'skybrite_z' : [2000, 5000],
+    'det_frac_g' : [0.75, 1.],
+    'det_frac_r' : [0.75, 1.],
     'det_frac_i' : [0.75, 1.],
+    'det_frac_z' : [0.75, 1.],
     'sig_zp_g' : [.005, .015],
     'sig_zp_r' : [.005, .015],
     'sig_zp_i' : [.005, .015],
     'sig_zp_z' : [.005, .015],
-    'exp_time_i' : [0, 800]
+    'exp_time_g' : [0, 800],
+    'exp_time_r' : [0, 800],
+    'exp_time_i' : [0, 800],
+    'exp_time_z' : [0, 800]
 }
 
 density_dx = {
@@ -34,14 +46,25 @@ density_dx = {
     'depth_r': 0.5,
     'depth_i': 0.5,
     'depth_z': 0.5,
+    'skyvar_g' : 0.25,
+    'skyvar_r' : 0.25,
     'skyvar_i' : 0.25,
+    'skyvar_z' : 0.25,
+    'skybrite_g' : 100,
+    'skybrite_r' : 100,
     'skybrite_i' : 100,
+    'det_frac_g' : .05,
+    'det_frac_r' : .05,
     'det_frac_i' : .05,
+    'det_frac_z' : .05,
     'sig_zp_g' : .0005,
     'sig_zp_r' : .0005,
     'sig_zp_i' : .0005,
     'sig_zp_z' : .0005,
-    'exp_time_i' : 25
+    'exp_time_g' : 25,
+    'exp_time_r' : 25,
+    'exp_time_i' : 25,
+    'exp_time_z' : 25
 }
 
 trend_xlim = {
@@ -53,18 +76,30 @@ trend_xlim = {
     'airmass_r' : [1., 1.4],
     'airmass_i' : [1., 1.4],
     'airmass_z' : [1., 1.4],
+    'skyvar_g' : [5, 15],
+    'skyvar_r' : [5, 15],
     'skyvar_i' : [5, 15],
+    'skyvar_z' : [5, 15],
     'depth_g' : [22, 24],
     'depth_r' : [22, 24],
     'depth_i' : [22, 24],
     'depth_z' : [22, 24],
+    'skybrite_g' : [2000, 5000],
+    'skybrite_r' : [2000, 5000],
     'skybrite_i' : [2000, 5000],
+    'skybrite_z' : [2000, 5000],
+    'det_frac_g' : [0.75, 1.],
+    'det_frac_r' : [0.75, 1.],
     'det_frac_i' : [0.75, 1.],
+    'det_frac_z' : [0.75, 1.],
     'sig_zp_g' : [.005, .015],
     'sig_zp_r' : [.005, .015],
     'sig_zp_i' : [.005, .015],
     'sig_zp_z' : [.005, .015],
-    'exp_time_i' : [0, 800]
+    'exp_time_g' : [0, 800],
+    'exp_time_r' : [0, 800],
+    'exp_time_i' : [0, 800],
+    'exp_time_z' : [0, 800]
 }
 
 trend_dx = {
@@ -80,17 +115,24 @@ trend_dx = {
     'depth_r': 0.5,
     'depth_i': 0.5,
     'depth_z': 0.5,
+    'skyvar_g' : 1,
+    'skyvar_r' : 1,
     'skyvar_i' : 1,
+    'skyvar_z' : 1,
+    'skybrite_g' : 250,
+    'skybrite_r' : 250,
     'skybrite_i' : 250,
+    'skybrite_z' : 250,
+    'det_frac_g' : .05,
+    'det_frac_r' : .05,
     'det_frac_i' : .05,
+    'det_frac_z' : .05,
     'sig_zp_g' : .00125,
     'sig_zp_r' : .00125,
     'sig_zp_i' : .00125,
     'sig_zp_z' : .00125,
-    'exp_time_i' : 100
+    'exp_time_g' : 100,
+    'exp_time_r' : 100,
+    'exp_time_i' : 100,
+    'exp_time_z' : 100
 }
-
-cols = ['meas_cm_g_1', 'meas_cm_g_2', 'meas_cm_T', 'meas_cm_fracdev', 'meas_cm_TdByTe', 'meas_cm_flux_s2n_i']
-
-mag_colname = 'meas_cm_mag_deredden'
-# mag_colname = 'meas_cm_mag'
