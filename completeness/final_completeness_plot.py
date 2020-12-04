7a5f794fbc0d879e125a1c21a13ffe0af5c61421
emhuff <emhuff@des90.fnal.gov>
2020-12-04 17:03:21 -0600

    changed area to match error bars for SN deep field

--- completeness/final_completeness_plot.py
+++ completeness/final_completeness_plot.py
@@ -11,20 +11,22 @@ if __name__ == '__main__':
     fig,ax = plt.subplots(nrows=1,ncols=1,figsize=(7,5))
     ntiles = 1493
     njack = 50
+    fA = np.sqrt(0.5/2.68) # sqrt ratio of tile area to deep field area
+
     # g:
-    ax.errorbar(data['bin_centers'],data['band_detfracs'][0,:],data['detfrac_err']*np.sqrt(ntiles*1./njack),label='g',marker='o',color='blue',fmt='s')
+    ax.errorbar(data['bin_centers'],data['band_detfracs'][0,:],data['detfrac_err']*np.sqrt(ntiles*1./njack)*fA,label='g',marker='o',color='blue',fmt='s')
     xdeep_g,detfrac_deep_g = np.loadtxt("deep_g.dat",unpack=True,skiprows=1)
     ax.plot(xdeep_g,detfrac_deep_g,color='blue')
 
-    ax.errorbar(data['bin_centers'],data['band_detfracs'][1,:],data['detfrac_err']*np.sqrt(ntiles*1./njack),label='r',marker='o',color='green',fmt='s')
+    ax.errorbar(data['bin_centers'],data['band_detfracs'][1,:],data['detfrac_err']*np.sqrt(ntiles*1./njack)*fA,label='r',marker='o',color='green',fmt='s')
     xdeep_r,detfrac_deep_r = np.loadtxt("deep_r.dat",unpack=True,skiprows=1)
     ax.plot(xdeep_r,detfrac_deep_r,color='green')
 
-    ax.errorbar(data['bin_centers'],data['band_detfracs'][2,:],data['detfrac_err']*np.sqrt(ntiles*1./njack),label='i',marker='o',color='orange',fmt='s')
+    ax.errorbar(data['bin_centers'],data['band_detfracs'][2,:],data['detfrac_err']*np.sqrt(ntiles*1./njack)*fA,label='i',marker='o',color='orange',fmt='s')
     xdeep_i,detfrac_deep_i = np.loadtxt("deep_i.dat",unpack=True,skiprows=1)
     ax.plot(xdeep_i,detfrac_deep_i,color='orange')
 
-    ax.errorbar(data['bin_centers'],data['band_detfracs'][3,:],data['detfrac_err']*np.sqrt(ntiles*1./njack),label='z',marker='o',color='red',fmt='s')
+    ax.errorbar(data['bin_centers'],data['band_detfracs'][3,:],data['detfrac_err']*np.sqrt(ntiles*1./njack)*fA,label='z',marker='o',color='red',fmt='s')
     xdeep_z,detfrac_deep_z = np.loadtxt("deep_z.dat",unpack=True,skiprows=1)
     ax.plot(xdeep_z,detfrac_deep_z,color='red')
 
